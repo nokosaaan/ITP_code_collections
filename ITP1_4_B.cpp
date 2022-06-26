30e4bd25b93d8c029defe94c5d2cf779e16bd5bb
noko <nishimura.r.019@ms.saitama-u.ac.jp>
2022-06-26 16:23:03 +0900

    1-4-Bをアップロード

---
+++ ITP1_4_B.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double p = 3.14159265358979;
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(6) << p*r*r << " " << fixed << setprecision(6) << 2*p*r << endl;
+}
+/*感想：簡単、1-4-Aが解けていれば問題なし*/
