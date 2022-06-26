4f646de4e05bec842c304bc105147052fbfac330
noko <nishimura.r.019@ms.saitama-u.ac.jp>
2022-06-26 15:53:13 +0900

    1-3-Aをアップロード

---
+++ ITP1_3_A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=0; i<1000; i++){
+        cout << "Hello World" << endl;
+    }
+}
+/*感想：簡単、Hello Worldを1000回
+        表示するだけの簡単なお仕事*/
